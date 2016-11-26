a5fd084051521d8c0f11040c143a81c1c493e474
fmv <djkang2008@daum.net>
2016-11-26 16:41:26 +0900

    새로운 폼으로 정리해

--- trySeaborn.py
+++ trySeaborn.py
@@ -1,36 +1,22 @@
 
 # coding: utf-8
-
-# In[4]:
-
-
+# 새로운 폼으로 정리를 시도
 get_ipython().magic(u'matplotlib inline')
-
 import seaborn
-
-
-# In[5]:
-
 import seaborn as sns
 
 # Load one of the data sets that come with seaborn
+# 데모를 위해 디폴트 데이터를 가져와서 보여준다. 
 tips = sns.load_dataset("tips")
-
 sns.jointplot("total_bill", "tip", tips, kind='reg');
 
-
-# In[12]:
-
+# 상위 몇 개의 데이터만 출력
 tips.head()
 
 
-# In[11]:
-
+# 데이터의 긾이 출력
 print len(tips), 
 print len(tips)
-
-
-# In[ ]:
 
 
 
